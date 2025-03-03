511d4cc57fdec38e33ec78e1bd6fcfa1ffff6f94
mousty00 <moustapha.dev03@gmail.com>
2025-03-03 09:53:11 +0100

    Initial Commit

---
+++ GymBro/GymBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroApp.swift
+//  GymBro
+//
+//  Created by Moustapha Ndiaye on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GymBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymBroTests/GymBroTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroTests.swift
+//  GymBroTests
+//
+//  Created by Moustapha Ndiaye on 03/03/25.
+//
+
+import Testing
+@testable import GymBro
+
+struct GymBroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
